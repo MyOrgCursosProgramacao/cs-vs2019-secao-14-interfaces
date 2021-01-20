460b9f2d6f646a0289f111a9fe3ca533a93118cb
Diego <alvarodiego@gmail.com>
2021-01-20 14:24:09 -0300

    Classe veículo criada

---
+++ src/Entities/Veiculo.cs
@@ -0,0 +1,12 @@
+﻿namespace src.Entities
+{
+    class Veiculo
+    {
+        public string Modelo { get; private set; }
+
+        public Veiculo(string modelo)
+        {
+            Modelo = modelo;
+        }
+    }
+}
