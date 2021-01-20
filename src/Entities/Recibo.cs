8b0506117ee5c4662e4ce6f396d166274a8b6f83
Diego <alvarodiego@gmail.com>
2021-01-20 14:32:23 -0300

    Classe Recibo criada

---
+++ src/Entities/Recibo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace src.Entities
+{
+    class Recibo
+    {
+        public double ValorDoServico { get; private set; }
+        public double Imposto { get; private set; }
+
+        public double PagamentoTotal
+        {
+            get { return ValorDoServico + Imposto; }
+        }
+
+        public Recibo(double valorDoServico, double imposto)
+        {
+            ValorDoServico = valorDoServico;
+            Imposto = imposto;
+        }
+
+        public override string ToString()
+        {
+            return "Serviços: R$ "
+                + ValorDoServico.ToString("F2", CultureInfo.InvariantCulture)
+                + Environment.NewLine
+                + "Impostos: R$ "
+                + Imposto.ToString("F2", CultureInfo.InvariantCulture)
+                + Environment.NewLine
+                + "Total: R$ "
+                + PagamentoTotal.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
